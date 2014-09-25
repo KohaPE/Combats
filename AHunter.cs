858f88598f8033d83be9dcaa209b74a43a6b6809
Koha <kohatx3@gmail.com>
2014-09-25 16:25:56 +1000

    Range Check on BM spec

--- AHunter.cs
+++ AHunter.cs
@@ -316,7 +316,7 @@ if (ME.HasAuraById((int)Auras.BMCheck))
                 return;
             }
 		//KillCommand
-		if(AI.Controllers.Spell.CanCast((int)Spells.KillCommand)&& Pet.Position.Distance3DFromPlayer < 25)
+		if(AI.Controllers.Spell.CanCast((int)Spells.KillCommand)&& TARGET.Position.Distance3DFromPlayer < 39)
 		    {
                 WoW.Internals.ActionBar.ExecuteSpell((int)Spells.KillCommand);
                 return;
